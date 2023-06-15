77affb48f539cdf60d02515f4b3b1c78f332bdbf
Adam Blevins <blevinspercussion@gmail.com>
2023-06-15 09:45:46 -0400

    Initial commit. Removed unnecessary boilerplate

---
+++ app/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+import React from "react";
+
+const Home = () => {
+  return <div>Home</div>;
+};
+
+export default Home;
