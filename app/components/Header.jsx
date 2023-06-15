1ee6f10d4054a4df3873a36d88052081f55bb238
Adam Blevins <blevinspercussion@gmail.com>
2023-06-15 10:27:23 -0400

    Search bar is in place, which a basic function to store input

---
+++ app/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div className="header">
+      <h1>Personal Movie Database</h1>
+    </div>
+  );
+};
+
+export default Header;
--- app/page.js
+++ app/page.js
@@ -1,10 +1,36 @@
+"use client";
+
 import Image from "next/image";
 import styles from "./page.module.css";
+import { useState, useEffect } from "react";
+
+import Header from "./components/Header";
 
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  const api_key = process.env.OMDB_API_KEY;
+
+  const [searchString, setSearchString] = useState("");
+
+  const handleSearchInput = () => {
+    setSearchString();
+  };
+
+  return (
+    <div>
+      <Header />
+      <hr />
+      <form>
+        <input
+          type="text"
+          onChange={(e) => setSearchString(e.target.value)}
+        ></input>
+        <button className="btn">Search</button>
+      </form>
+      <h1>{searchString}</h1>
+    </div>
+  );
 };
 
 export default Home;
